05af10a1f84d0eaf7a54183b9dfb5eb43e39d40a
Awwal Bankole <lawybanx35@gmail.com>
2018-12-14 12:04:17 +0100

    Countdown Timer

---
+++ countdown.js
@@ -0,0 +1,21 @@
+let button = document.querySelector("#button");
+let div = document.querySelector("div")
+let Timer;
+button.addEventListener('click', function countDown(){
+let time = document.querySelector("#input").value;
+    if(time != 0){
+        clearInterval(Timer)
+    }
+    Timer = setInterval(function(){
+        time--;
+        document.getElementById("display").textContent = time;
+        if(time <= 0){
+        clearInterval(Timer)};
+    },1000);
+    
+}
+)
+// function removeItem(time) {
+//     time.target.parentElement.removeChild(time.target);
+//   };
+// div.onclick = removeItem;
