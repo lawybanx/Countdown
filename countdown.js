19fb8bec6cf5f40de3e3a665d8af353e4f0de01f
Awwal Bankole <lawybanx35@gmail.com>
2018-12-16 21:49:49 +0100

    Used jslint

--- countdown.js
+++ countdown.js
@@ -1,29 +1,32 @@
-let button = document.querySelector("#button");
-let div = document.querySelector("div")
-let Timer;
-let time;
-button.addEventListener('click', function countDown(){
-let time = document.querySelector("#input").value;
-    if(time != 0){
-        clearInterval(Timer)
+var button = document.querySelector("#button");
+var div = document.querySelector("div");
+var Timer;
+var time;
+button.addEventListener('click', function countDown() {
+    'use strict';
+    var time = document.querySelector("#input").value;
+    if (time !== 0) {
+        clearInterval(Timer);
     }
-    Timer = setInterval(function(){
+    Timer = setInterval(function () {
         time--;
         document.getElementById("display").textContent = time;
-        if(time <= 0){
-        clearInterval(Timer)};
-    },1000);
+        if (time <= 0) {
+            clearInterval(Timer);
+        }
+    }, 1000);
     
 }
-)
-let reset = document.querySelector("#reset");
-reset.addEventListener('click', function reset(){
-	if(time != 0){
-        clearInterval(Timer)
-    };
-document.getElementById("display").innerHTML = 0;
+    );
+var reset = document.querySelector("#reset");
+reset.addEventListener('click', function reset() {
+	'use strict';
+    if (time !== 0) {
+        clearInterval(Timer);
+    }
+    document.getElementById("display").innerHTML = 0;
 
-})
+});
 // function removeItem(time) {
 //     time.target.parentElement.removeChild(time.target);
 //   };
