2606b106c03732286a4aae4a7652f5ff98894485
Awwal Bankole <lawybanx35@gmail.com>
2018-12-17 01:51:57 +0100

    Updated

--- countdown.js
+++ countdown.js
@@ -14,11 +14,14 @@ button.addEventListener('click', function () {
         if (time <= 0) {
             clearInterval(Timer);
         }
+        reset.style.display = 'inline-block';
+        pause.style.display = 'inline-block';
     }, 1000);
     
 }
     );
 var reset = document.querySelector("#reset");
+reset.style.display = 'none';
 reset.addEventListener('click', function reset() {
 	'use strict';
     if (time !== 0) {
@@ -27,3 +30,9 @@ reset.addEventListener('click', function reset() {
     document.getElementById("display").innerHTML = 0;
 
 });
+let pause = document.querySelector("#pause");
+pause.style.display = 'none';
+pause.addEventListener('click', function(){
+    'use-strict';
+    alert("Press ok to continue.");
+})
