596861c2012661f0d025d10a6136996d49577199
Awwal Bankole <lawybanx35@gmail.com>
2019-01-14 13:20:11 +0100

    updated

--- countdown.js
+++ countdown.js
@@ -1,5 +1,4 @@
 var button = document.querySelector("#button");
-var div = document.querySelector("div");
 var Timer;
 var time;
 button.addEventListener('click', function () {
@@ -11,42 +10,41 @@ button.addEventListener('click', function () {
     Timer = setInterval(function () {
         time--;
         document.getElementById("display").textContent = time;
-        if (time <= 0) {
+        if (time <= 0 || time == null) {
+            clearInterval(Timer);
+            alert("please input a number");
+        }
+        document.getElementById("display").innerHTML = 0;
+        pause.style.display = 'inline-block';
+        resume.style.display = 'inline-block';
+        reset.style.display = 'inline-block';
+        }, 1000);
+});
+
+    var pause = document.querySelector("#pause");
+    pause.style.display = 'none';
+    pause.addEventListener('click', function () {
+        'use-strict';
+        if (time !== 0) {
             clearInterval(Timer);
         }
-        reset.style.display = 'inline-block';
-        pause.style.display = 'inline-block';
-        rest.style.display = 'inline-block';
-    }, 1000);
-    
-}
-    );
-var stop = document.querySelector("#reset");
-stop.style.display = 'none';
-stop.addEventListener('click', function reset() {
-	'use strict';
-    if (time !== 0) {
-        clearInterval(Timer);
-    }
-    
-});
-let resume = document.querySelector("#pause");
-resume.style.display = 'none';
-resume.addEventListener('click', function(){
-    'use-strict';
-    time = document.getElementById("display").textContent;
-     if (time !== 0) {
-        clearInterval(Timer);
-    }
-})
+    });
 
-var rest = document.querySelector("#rest");
-rest.style.display = 'none';
-rest.addEventListener('click', function rest() {
-	'use strict';
-    if (time !== 0) {
-        clearInterval(Timer);
-    }
-    document.getElementById("display").innerHTML = 0;
+    var resume = document.querySelector("#resume");
+    resume.style.display = 'none';
+    resume.addEventListener('click', function () {
+        'use strict';
+     
+        
+    });
 
-});
+    var reset = document.querySelector("#reset");
+    reset.style.display = 'none';
+    reset.addEventListener('click', function () {
+        'use strict';
+        if (time !== 0) {
+            clearInterval(Timer);
+        }
+        document.getElementById("display").innerHTML = 0;
+
+    });
