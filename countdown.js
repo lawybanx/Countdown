e0c3eae9f91145cf018effd2ad40407a94053d1f
Awwal Bankole <lawybanx35@gmail.com>
2019-01-16 09:53:58 +0100

    cleaned code a bit

--- countdown.js
+++ countdown.js
@@ -1,38 +1,62 @@
 var button = document.querySelector("#button");
 var Timer;
 var time;
-button.addEventListener('click', function () {
-    'use strict';
-    var time = document.querySelector("#input").value;
-    if (time !== 0) {
-        clearInterval(Timer);
-    }
-    Timer = setInterval(function () {
-        document.getElementById("display").textContent = time;
-        time--;
-        if ( time < 0 ) {
-            clearInterval(Timer);
-            document.getElementById("display").innerHTML = 0;
-        }
-        
-        pause.style.display = 'inline-block';
-        resume.style.display = 'inline-block';
-        reset.style.display = 'inline-block';
-        }, 1000);
-});
+    button.addEventListener('click', countdownFunc());
 
     var pause = document.querySelector("#pause");
     pause.style.display = 'none';
-    pause.addEventListener('click', function () {
+    pause.addEventListener('click', pauseFunc());
+
+    var resume = document.querySelector("#resume");
+    resume.style.display = 'none';
+    resume.addEventListener('click', resumeFunc());
+
+    var reset = document.querySelector("#reset");
+    reset.style.display = 'none';
+    reset.addEventListener('click', resetFunc());
+    
+function resetFunc() {
+    return function () {
+        'use strict';
+        if (time !== 0) {
+            clearInterval(Timer);
+        }
+        document.getElementById("display").innerHTML = 0;
+    };
+}
+
+function pauseFunc() {
+    return function () {
         'use-strict';
         if (time !== 0) {
             clearInterval(Timer);
         }
-    });
+    };
+}
 
-    var resume = document.querySelector("#resume");
-    resume.style.display = 'none';
-    resume.addEventListener('click', function () {
+function countdownFunc() {
+    return function () {
+        'use strict';
+        var time = document.querySelector("#input").value;
+        if (time !== 0) {
+            clearInterval(Timer);
+        }
+        Timer = setInterval(function () {
+            document.getElementById("display").textContent = time;
+            time--;
+            if (time < 0) {
+                clearInterval(Timer);
+                document.getElementById("display").innerHTML = 0;
+            }
+            pause.style.display = 'inline-block';
+            resume.style.display = 'inline-block';
+            reset.style.display = 'inline-block';
+        }, 1000);
+    };
+}
+
+function resumeFunc() {
+    return function () {
         'use strict';
         var time = document.querySelector("#display").textContent;
         if (time !== 0) {
@@ -41,24 +65,11 @@ button.addEventListener('click', function () {
         Timer = setInterval(function () {
             time--;
             document.getElementById("display").textContent = time;
-            if ( time < 0 ) {
+            if (time < 0) {
                 clearInterval(Timer);
                 document.getElementById("display").innerHTML = 0;
             }
-            
-            pause.style.display = 'inline-block';
-            resume.style.display = 'inline-block';
-            reset.style.display = 'inline-block';
-            }, 1000);
-});
+        }, 1000);
+    };
+}
 
-    var reset = document.querySelector("#reset");
-    reset.style.display = 'none';
-    reset.addEventListener('click', function () {
-        'use strict';
-        if (time !== 0) {
-            clearInterval(Timer);
-        }
-        document.getElementById("display").innerHTML = 0;
-
-    });
